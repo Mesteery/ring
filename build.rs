94918862075465442977b131b74f3d94cdfe4437
Mestery <mestery@protonmail.com>
2023-06-06 19:11:33 +0200

    Update build.rs

--- build.rs
+++ build.rs
@@ -323,13 +323,6 @@ struct Target {
 }
 
 fn build_c_code(target: &Target, pregenerated: PathBuf, out_dir: &Path) {
-    #[cfg(not(feature = "wasm32_c"))]
-    {
-        if &target.arch == "wasm32" {
-            return;
-        }
-    }
-
     let includes_modified = RING_INCLUDES
         .iter()
         .chain(RING_BUILD_FILE.iter())
